25eb0ce1fe365ce45cef024cfd89742f6bf2360d
unknown <noeliatinajero1996@gmail.com>
2023-11-15 12:18:16 +0100

    creamos la pagina de contacto y el formulario

--- view/ContactoView.php
+++ view/ContactoView.php
@@ -87,6 +87,20 @@
         Somos una empresa dedicada al campo de la informática desde hace más de 20 años y contamos con trabajadores con
         bastante experiencia en el sector
     </p>
+
+    <h3>Formulario de Contacto</h3>
+    <form method="POST" action="mail.php">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required><br><br>
+
+        <label for="correo">Correo electrónico:</label>
+        <input type="email" id="correo" name="correo" required><br><br>
+
+        <label for="mensaje">Mensaje:</label>
+        <textarea id="mensaje" name="mensaje" required></textarea><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 
 </html>
